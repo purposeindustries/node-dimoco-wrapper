2a5ea340d490e1829744df13de103c218437d03e
Bence Dányi <madbence@gmail.com>
2014-03-04 17:04:50 +0100

    Allow URL override

--- test/index.js
+++ test/index.js
@@ -1,23 +1,30 @@
 var lib = require('../');
 
-describe('App', function(){
+describe('App', function() {
   describe('#constructor', function() {
-    it('should set default ulr', function(){
+    it('should set default ulr', function() {
       var d = new lib.App({
         password: 'foobar'
       });
       d.url.should.equal('http://services.dimoco.at/psms/send-sms?');
     });
-    it('should accept password', function(){
+    it('should accept password', function() {
       var d = new lib.App({
         password: 'foobar'
       });
       d.password.should.equal('foobar');
     });
-    it('should require password', function(){
+    it('should require password', function() {
       (function(){
         new lib.App();
       }).should.throw();
+    });
+    it('should accept url override', function() {
+      var d = new lib.App({
+        password: 'foobar',
+        url: 'baz'
+      });
+      d.url.should.equal('baz');
     });
   });
 });
