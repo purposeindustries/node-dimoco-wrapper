ebede8b70288850a24565566bdef9d652330b3d7
Bence Dányi <madbence@gmail.com>
2014-03-04 17:00:59 +0100

    Password is required

--- test/index.js
+++ test/index.js
@@ -3,7 +3,9 @@ var lib = require('../');
 describe('App', function(){
   describe('#constructor', function() {
     it('should set default ulr', function(){
-      var d = new lib.App();
+      var d = new lib.App({
+        password: 'foobar'
+      });
       d.url.should.equal('http://services.dimoco.at/psms/send-sms?');
     });
     it('should accept password', function(){
@@ -11,6 +13,11 @@ describe('App', function(){
         password: 'foobar'
       });
       d.password.should.equal('foobar');
+    });
+    it('should require password', function(){
+      (function(){
+        new lib.App();
+      }).should.throw();
     });
   });
 });
