f5fed67edc8138adcf4e2235549f5fdc13ad8777
Bence Dányi <madbence@gmail.com>
2014-03-10 09:58:44 +0100

    Refactor tests

--- test/index.js
+++ test/index.js
@@ -3,16 +3,16 @@ var request = require('request');
 
 describe('App', function() {
   describe('#constructor', function() {
-    it('should set default ulr', function() {
-      var d = new lib.App({
+    var d;
+    beforeEach(function() {
+      d = new lib.App({
         password: 'foobar'
       });
+    });
+    it('should set default ulr', function() {
       d.url.should.equal('http://services.dimoco.at/psms/send-sms?');
     });
     it('should accept password', function() {
-      var d = new lib.App({
-        password: 'foobar'
-      });
       d.password.should.equal('foobar');
     });
     it('should require password', function() {
@@ -21,20 +21,17 @@ describe('App', function() {
       }).should.throw();
     });
     it('should accept url override', function() {
-      var d = new lib.App({
+      d = new lib.App({
         password: 'foobar',
         url: 'baz'
       });
       d.url.should.equal('baz');
     });
     it('should use `request` by default', function() {
-      var d = new lib.App({
-        password: 'foobar'
-      });
       d.request.should.equal(request);
     });
     it('should accept `request` override', function() {
-      var d = new lib.App({
+      d = new lib.App({
         password: 'foobar',
         request: 'foo'
       });
