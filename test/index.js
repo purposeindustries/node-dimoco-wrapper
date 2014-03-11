c79574bfe99b425327f668487815f4d46d800ed1
Bence Dányi <madbence@gmail.com>
2014-03-11 12:00:41 +0100

    Refactor tests

--- test/index.js
+++ test/index.js
@@ -1,18 +1,18 @@
 var lib = require('../');
 
 describe('App', function() {
+  var d, conf = {
+    password: 'foo', appId: 'bar'
+  };
   describe('#constructor', function() {
-    var d;
     beforeEach(function() {
-      d = new lib.App({
-        password: 'foobar'
-      });
+      d = new lib.App(conf);
     });
     it('should set default ulr', function() {
       d.url.should.equal('http://services.dimoco.at/psms/send-sms?');
     });
     it('should accept password', function() {
-      d.password.should.equal('foobar');
+      d.password.should.equal('foo');
     });
     it('should require password', function() {
       (function(){
@@ -32,6 +32,7 @@ describe('App', function() {
     it('should accept url override', function() {
       d = new lib.App({
         password: 'foobar',
+        appId: 'bar',
         url: 'baz'
       });
       d.url.should.equal('baz');
@@ -42,17 +43,15 @@ describe('App', function() {
     it('should accept `request` override', function() {
       d = new lib.App({
         password: 'foobar',
+        appId: 'baz',
         request: 'foo'
       });
       d.request.should.equal('foo');
     });
   });
   describe('#isValidMO', function() {
-    var d;
     beforeEach(function() {
-      d = new lib.App({
-        password: 'foobar'
-      });
+      d = new lib.App(conf);
     });
     it('should return bool', function() {
       (typeof d.isValidMO({})).should.equal('boolean')
